167e5bf619631a8018b867cf1d71d5f00e4cf42e
XenDQ <xeanxyrone@gmail.com>
2026-04-07 13:14:51 +0800

    feat: added boards, cards, and lists modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { CardsModule } from './cards/cards.module';
+import { ListsModule } from './lists/lists.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule, CardsModule, ListsModule],
   controllers: [AppController],
   providers: [AppService],
 })

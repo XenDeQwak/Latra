6a0995a1520876cbeee0f95a0c39de06618cc6ec
XenDQ <xeanxyrone@gmail.com>
2026-04-07 16:20:32 +0800

    feat: deleted app.service and controller and updated app.module and main

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
 import { CardsModule } from './cards/cards.module';
 import { ListsModule } from './lists/lists.module';
 
 @Module({
   imports: [BoardsModule, CardsModule, ListsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  })
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

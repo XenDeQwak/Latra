b7f05c0ce47595fb29c65e231f9bab2a7d683dc5
XenDQ <xeanxyrone@gmail.com>
2026-04-07 17:30:57 +0800

    feat: added auth service module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { BoardsModule } from './boards/boards.module';
 import { CardsModule } from './cards/cards.module';
 import { ListsModule } from './lists/lists.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardsModule, CardsModule, ListsModule, UsersModule],
+  imports: [BoardsModule, CardsModule, ListsModule, UsersModule, AuthModule],
 })
 export class AppModule {}
